75ab7c3f66c110223240f43b5b0d56b86a40edd5
0xKaBG <minh20051202@example.com>
2025-11-01 22:08:59 +0700

    new commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,13 @@ const routes = require("./src/app/http/routes/index");
 
 module.exports = function createServer() {
   const app = express();
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      methods: ["GET"],
+      credentials: false,
+    })
+  );
   app.use(express.json());
   app.use("/api", routes);
   return app;
